0236e93a73e119e00935439a8fe0fb5ba32d33f1
Leandro Melgarejo <leandromelgarejo@hotmail.com>
2022-12-18 20:20:48 -0300

    changes

--- app/Util/serialNumber.ts
+++ app/Util/serialNumber.ts
@@ -3,7 +3,7 @@ export async function createSerialNumber() {
     const randomNumber = Math.floor(1000000 + Math.random() * 9000000)
     const characters = 'RWEQUIYTPOFASDJHKGLVXCZNBMeqwryutiopfsdahjkglvcxzbnm'
 
-    for(let i; i < 4; i++) {
+    for(let i = 0; i < 4; i++) {
         char += characters.charAt(Math.floor(Math.random() * characters.length))
     }
 
